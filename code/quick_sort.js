d901a55f7eba59b5292adae14a494c8e1daea7d7
向前Hell <oncwnuIr26oMvzC0xdaTvJ2-auwA@git.weixin.qq.com>
2019-12-07 09:51:34 +0800

    update

--- code/quick_sort.js
+++ code/quick_sort.js
@@ -28,6 +28,7 @@ export default class QuickSort {
   quick_sort = (arr, first, last) => {
     if (first >= last) return;
     let [i, j] = [first, last];
+    let key = first;
     let temp = arr[first];
     while (i < j) {
       while (i < j && temp < arr[j]) {
@@ -39,6 +40,7 @@ export default class QuickSort {
       }
       arr[j] = arr[i];
     }
+    key = i;
     arr[i] = temp;
 
     console.log('arr:', arr);
@@ -52,4 +54,25 @@ export default class QuickSort {
     this.quick_sort(arr, 0, len - 1);
     console.log('result arr:', arr)
   }
+}
+
+// 三路快排
+function quick_sort(arr) {
+  const len = arr.length
+  if (len === 0) return []
+  let left = []
+  let right = []
+  let center = []
+  const middle = parseInt(len/2, 10)
+  let pivot = Math.min(Math.max(arr[0], arr[middle]), Math.max(arr[len-1], arr[middle]))
+  for (let i = 0; i < len; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i])
+    } else if (arr[i] === pivot) {
+      center.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
+  return [...quick_sort(left), ...center, ...quick_sort(right)]
 }
